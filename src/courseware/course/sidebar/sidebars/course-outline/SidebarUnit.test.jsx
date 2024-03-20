58666690c1a775468f3e94014dcff35b8aef1d54
ihor-romaniuk <ihor.romaniuk@raccoongang.com>
2024-03-20 15:45:24 +0100

    fix: extend tests

--- src/courseware/course/sidebar/sidebars/course-outline/SidebarUnit.test.jsx
+++ src/courseware/course/sidebar/sidebars/course-outline/SidebarUnit.test.jsx
@@ -1,62 +1,70 @@
+import { AppProvider } from '@edx/frontend-platform/react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
 
+import { initializeMockApp, initializeTestStore } from '../../../../../setupTest';
 import SidebarUnit from './SidebarUnit';
 
-const unit = {
-  complete: false,
-  icon: 'video',
-  id: 'unit1',
-  title: 'Unit 1',
-  type: 'video',
-};
-
-const RootWrapper = (props) => (
-  <IntlProvider locale="en">
-    <MemoryRouter>
-      <SidebarUnit
-        isFirst
-        id="unit1"
-        courseId="course123"
-        sequenceId="sequence123"
-        unit={unit}
-        isActive={false}
-        {...props}
-      />
-    </MemoryRouter>
-  </IntlProvider>
-);
+initializeMockApp();
 
 describe('<SidebarUnit />', () => {
-  it('renders correctly when unit is incomplete', () => {
-    const {
-      getByText,
-      container,
-    } = render(<RootWrapper />);
+  let store = {};
+  const unit = {
+    complete: false,
+    icon: 'video',
+    id: 'unit1',
+    title: 'Unit 1',
+    type: 'video',
+  };
 
-    expect(getByText(unit.title)).toBeInTheDocument();
+  const initTestStore = async (options) => {
+    store = await initializeTestStore(options);
+  };
+
+  function renderWithProvider(props = {}) {
+    const { container } = render(
+      <AppProvider store={store} wrapWithRouter={false}>
+        <IntlProvider locale="en">
+          <MemoryRouter>
+            <SidebarUnit
+              isFirst
+              id="unit1"
+              courseId="course123"
+              sequenceId="sequence123"
+              unit={unit}
+              isActive={false}
+              {...props}
+            />
+          </MemoryRouter>
+        </IntlProvider>
+      </AppProvider>,
+    );
+    return container;
+  }
+
+  it('renders correctly when unit is incomplete', async () => {
+    await initTestStore();
+    const container = renderWithProvider();
+
+    expect(screen.getByText(unit.title)).toBeInTheDocument();
     expect(container.querySelector('.text-success')).not.toBeInTheDocument();
   });
 
-  it('renders correctly when unit is complete', () => {
-    const {
-      getByText,
-      container,
-    } = render(<RootWrapper unit={{ ...unit, complete: true }} />);
+  it('renders correctly when unit is complete', async () => {
+    await initTestStore();
+    const container = renderWithProvider({ unit: { ...unit, complete: true } });
 
-    expect(getByText(unit.title)).toBeInTheDocument();
+    expect(screen.getByText(unit.title)).toBeInTheDocument();
     expect(container.querySelector('.text-success')).toBeInTheDocument();
     expect(container.querySelector('.border-top')).not.toBeInTheDocument();
   });
 
-  it('renders correctly when unit is not first', () => {
-    const {
-      getByText,
-      container,
-    } = render(<RootWrapper isFirst={false} />);
+  it('renders correctly when unit is not first', async () => {
+    await initTestStore();
+    const container = renderWithProvider({ isFirst: false });
 
-    expect(getByText(unit.title)).toBeInTheDocument();
+    expect(screen.getByText(unit.title)).toBeInTheDocument();
     expect(container.querySelector('.border-top')).toBeInTheDocument();
   });
 });
