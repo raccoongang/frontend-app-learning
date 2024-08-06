28496d44fd05236dc3c32cc320c45d8eb4ae9e62
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2024-08-06 15:13:09 +0300

    feat: [AsPu-616] improved accessibility for Show/Hide Accordions

--- src/course-home/outline-tab/OutlineTab.test.jsx
+++ src/course-home/outline-tab/OutlineTab.test.jsx
@@ -107,6 +107,24 @@ describe('Outline Tab', () => {
       expect(expandedSectionNode).toHaveAttribute('aria-expanded', 'true');
     });
 
+    it('displays correct heading for expanded section', async () => {
+      const { courseBlocks } = await buildMinimalCourseBlocks(courseId, 'Title', { resumeBlock: true });
+      setTabData({ course_blocks: { blocks: courseBlocks.blocks } });
+      await fetchAndRender();
+      const headingContent = screen.getByText('Title of Section');
+      const { parentElement } = headingContent;
+      expect(parentElement.tagName).toBe('H2');
+    });
+
+    it('checks that the expanded section is within the correct list', async () => {
+      const { courseBlocks } = await buildMinimalCourseBlocks(courseId, 'Title', { resumeBlock: true });
+      setTabData({ course_blocks: { blocks: courseBlocks.blocks } });
+      await fetchAndRender();
+      const listElement = screen.getByRole('presentation', { id: 'courseHome-outline' });
+      expect(listElement).toBeInTheDocument();
+      expect(listElement.tagName).toBe('OL');
+    });
+
     it('handles expand/collapse all button click', async () => {
       await fetchAndRender();
       // Button renders as "Expand All"
